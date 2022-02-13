ce10e01f0aa781f4563fbb5a045f51f0020791b0
Parksubo <subo0521@hanmail.net>
2022-02-13 17:11:43 +0900

    react bootstrap added

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,11 @@ import getWeb3 from "./getWeb3";
 
 import "./App.css";
 
+// 이미지 import
+import logo from "./logo.svg";
+
 class App extends Component {
+/*
   state = { storageValue: 0, web3: null, accounts: null, contract: null };
 
   componentDidMount = async () => {
@@ -47,24 +51,19 @@ class App extends Component {
     // Update state with the result.
     this.setState({ storageValue: response });
   };
-
+*/
   render() {
+  /*
     if (!this.state.web3) {
       return <div>Loading Web3, accounts, and contract...</div>;
     }
+  */
     return (
       <div className="App">
-        <h1>Good to Go!</h1>
-        <p>Your Truffle Box is installed and ready.</p>
-        <h2>Smart Contract Example</h2>
-        <p>
-          If your contracts compiled and migrated successfully, below will show
-          a stored value of 5 (by default).
-        </p>
-        <p>
-          Try changing the value stored on <strong>line 42</strong> of App.js.
-        </p>
-        <div>The stored value is: {this.state.storageValue}</div>
+        <header>
+            <img src = {logo} className="App-logo"/>
+            <h1>Hello, React!</h1>
+        </header>
       </div>
     );
   }
